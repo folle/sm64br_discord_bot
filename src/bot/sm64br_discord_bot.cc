deafc680f44f5afc746b84cc683f2be94b0c6afc
Folle <folle@outlook.com>
2023-11-24 21:06:20 -0300

    Better logging and fix for streaming users not getting their role assigned. Some refactoring.

--- src/bot/sm64br_discord_bot.cc
+++ src/bot/sm64br_discord_bot.cc
@@ -22,6 +22,8 @@ namespace {
 
 
 Sm64brDiscordBot::Sm64brDiscordBot() {
+  logger_->info("Initializing bot");
+
   bot_.on_log(std::bind(&Sm64brDiscordBot::OnLog, this, std::placeholders::_1));
   bot_.on_ready(std::bind(&Sm64brDiscordBot::OnReady, this, std::placeholders::_1));
   //bot_.on_message_create(std::bind(&Sm64brDiscordBot::OnMessageCreate, this, std::placeholders::_1));
@@ -33,6 +35,7 @@ Sm64brDiscordBot::Sm64brDiscordBot() {
 }
 
 Sm64brDiscordBot::~Sm64brDiscordBot() {
+  logger_->info("Bot terminated");
   logger_->flush();
 }
 
@@ -98,65 +101,72 @@ void Sm64brDiscordBot::OnPresenceUpdate(const dpp::presence_update_t& presence_u
   ::ClearFinishedFutures(presence_update_futures_);
 
   presence_update_futures_.push_back(std::async(std::launch::async, [this, presence_update]() {
-    std::string streaming_user_name;
+    dpp::user* streaming_user = {};
     try {
-      auto const streaming_user = bot_.guild_get_member_sync(presence_update.rich_presence.guild_id, presence_update.rich_presence.user_id).get_user();
-      streaming_user_name = ((streaming_user == nullptr) ? "" : streaming_user->format_username());
+      streaming_user = bot_.guild_get_member_sync(database_.GetServerGuildId(), presence_update.rich_presence.user_id).get_user();
     } catch (const dpp::rest_exception& rest_exception) {
-      logger_->error("Failed to get user name. Exception description: {}", rest_exception.what());
-      //TODO
+      logger_->error("Failed to get member while processing presence update. Exception: '{}'", rest_exception.what());
+      return;
     }
 
+    if (!streaming_user) {
+      logger_->error("Streaming user wasn't found in the cache while processing presence update");
+      return;
+    }
+
+    const auto& streaming_user_name = streaming_user->format_username();
     const auto& activies = presence_update.rich_presence.activities;
     const auto streaming_activity = std::find_if(activies.cbegin(), activies.cend(), [](const dpp::activity& activity) {
       return (activity.type == dpp::activity_type::at_streaming) && (activity.name == "Twitch") && (activity.state == "Super Mario 64");
     });
+    const auto is_streaming_sm64 = activies.cend() != streaming_activity;
+
+    const auto& streaming_user_id = presence_update.rich_presence.user_id;
 
     const std::lock_guard<std::mutex> lock(on_presence_update_mutex_);
 
-    const auto it_user_id_and_message_id = streaming_users_ids_and_messages_ids_.find(presence_update.rich_presence.user_id);
-    const auto ping_sent_ = (streaming_users_ids_and_messages_ids_.cend() != it_user_id_and_message_id);
-    if (activies.cend() == streaming_activity) {
-      if (ping_sent_) {
-        logger_->info("User '{}', but ping was already sent", streaming_user_name);
+    const auto it_user_id_and_message_id = streaming_users_ids_and_messages_ids_.find(streaming_user_id);
+    const auto streaming_message_sent_ = (streaming_users_ids_and_messages_ids_.cend() != it_user_id_and_message_id);
+    if (streaming_message_sent_) {
+      if (!is_streaming_sm64) {
+        logger_->info("User '{}' finished streaming Super Mario 64", streaming_user_name);
 
-        bot_.message_delete_sync(it_user_id_and_message_id->second, database_.GetServerChannelId(Database::ServerChannels::kStreams));
+        bot_.message_delete(it_user_id_and_message_id->second, database_.GetServerChannelId(Database::ServerChannels::kStreams));
+        bot_.guild_member_remove_role(database_.GetServerGuildId(), streaming_user_id, database_.GetServerRoleId(Database::ServerRoles::kStreaming));
+
         streaming_users_ids_and_messages_ids_.erase(it_user_id_and_message_id);
       }
 
       return;
     }
+    
+    logger_->info("User '{}' started streaming Super Mario 64", streaming_user_name);
 
-    if (ping_sent_) {
-      logger_->info("Found Twitch streaming presence update for user '{}', but ping was already sent", streaming_user_name);
+    const auto streaming_message = dpp::message(database_.GetServerChannelId(Database::ServerChannels::kStreams),
+                                           fmt::format("**{}** estÃ¡ ao vivo jogando Super Mario 64! Assista em: **{}**", streaming_user_name, streaming_activity->url));
+    dpp::snowflake streaming_message_id = {};
+    try {
+      streaming_message_id = bot_.message_create_sync(streaming_message).id;
+    } catch (const dpp::rest_exception& rest_exception) {
+      logger_->error("Failed to create streaming message while processing presence update. Exception: '{}'", rest_exception.what());
       return;
     }
-    
-    logger_->info("Found Twitch streaming presence update for user '{}'. Sending ping", streaming_user_name);
 
-    const auto ping_message = dpp::message(database_.GetServerChannelId(Database::ServerChannels::kStreams),
-                                           fmt::format("**{}** estÃ¡ ao vivo jogando Super Mario 64! Assista em: **{}**", streaming_user_name, streaming_activity->url));
-    try {
-      const auto ping_message_id = bot_.message_create_sync(ping_message).id;
-      streaming_users_ids_and_messages_ids_[presence_update.rich_presence.user_id] = ping_message_id;
-    }
-    catch (const dpp::rest_exception& rest_exception) {
-      // TODO
-    }
+    bot_.guild_member_add_role(database_.GetServerGuildId(), streaming_user_id, database_.GetServerRoleId(Database::ServerRoles::kStreaming));
 
-    logger_->info("Finished processing presence update for user '{}'", streaming_user_name);
+    streaming_users_ids_and_messages_ids_[streaming_user_id] = streaming_message_id;
   }));
 }
 
 void Sm64brDiscordBot::OnGuildMemberAdd(const dpp::guild_member_add_t& guild_member_add) noexcept {
   const auto join_message = dpp::message(database_.GetServerChannelId(Database::ServerChannels::kUpdates),
-                                         fmt::format("{} acabou de entrar no servidor.", guild_member_add.added.get_user()->format_username()));
+                                         fmt::format("**@{}** acabou de entrar no servidor.", guild_member_add.added.get_user()->format_username()));
   bot_.message_create(join_message);
 }
 
 void Sm64brDiscordBot::OnGuildMemberRemove(const dpp::guild_member_remove_t& guild_member_remove) noexcept {
   const auto leave_message = dpp::message(database_.GetServerChannelId(Database::ServerChannels::kUpdates),
-                                          fmt::format("{} acabou de sair no servidor.", guild_member_remove.removed.format_username()));
+                                          fmt::format("**@{}** acabou de sair no servidor.", guild_member_remove.removed.format_username()));
   bot_.message_create(leave_message);
 }
 
@@ -165,11 +175,19 @@ void Sm64brDiscordBot::OnReady(const dpp::ready_t& ready) {
 }
 
 void Sm64brDiscordBot::ClearStreamingStatus() noexcept {
+  // Clear streaming roles
   dpp::snowflake highest_member_id = {};
-  dpp::guild_member_map guild_members;
+  dpp::guild_member_map members;
   do {
-    guild_members = bot_.guild_get_members_sync(database_.GetServerGuildId(), 1000, highest_member_id);
-    for (auto& member : guild_members) {
+    try {
+      constexpr uint16_t kMaxMembersPerCall = 1000;
+      members = bot_.guild_get_members_sync(database_.GetServerGuildId(), kMaxMembersPerCall, highest_member_id);
+    } catch (const dpp::rest_exception& rest_exception) {
+      logger_->error("Failed to get members while clearing streaming status. Exception: '{}'", rest_exception.what());
+      throw;
+    }
+
+    for (auto& member : members) {
       if (highest_member_id < member.first) {
         highest_member_id = member.first;
       }
@@ -177,23 +195,29 @@ void Sm64brDiscordBot::ClearStreamingStatus() noexcept {
       const auto& roles = member.second.get_roles();
       const auto it_streaming_role =  std::find(roles.cbegin(), roles.cend(), database_.GetServerRoleId(Database::ServerRoles::kStreaming));
       if (it_streaming_role != roles.cend()) {
-        bot_.guild_member_remove_role_sync(database_.GetServerGuildId(), member.second.user_id, *it_streaming_role);
+        bot_.guild_member_remove_role(database_.GetServerGuildId(), member.second.user_id, database_.GetServerRoleId(Database::ServerRoles::kStreaming));
       }
     }
-  } while (!guild_members.empty());
+  } while (!members.empty());
 
-  dpp::snowflake earliest_streaming_message_id = std::numeric_limits<dpp::snowflake>::max();
-  const auto streams_channel_id = database_.GetServerChannelId(Database::ServerChannels::kStreams);
-  dpp::message_map streaming_messages = bot_.messages_get_sync(streams_channel_id, {}, {}, {}, 100);
+  // Clear streaming messages
+  auto earliest_streaming_message_id = std::numeric_limits<dpp::snowflake>::max();
+  dpp::message_map streaming_messages;
   do {
-    for (const auto& message : streaming_messages) {
-      if (message.first < earliest_streaming_message_id) {
-        earliest_streaming_message_id = message.first;
-      }
-
-      bot_.message_delete_sync(message.first, streams_channel_id);
+    try {
+      constexpr uint64_t kMaxMessagesPerCall = 100;
+      streaming_messages = bot_.messages_get_sync(database_.GetServerChannelId(Database::ServerChannels::kStreams), {}, {}, {}, kMaxMessagesPerCall);
+    } catch (const dpp::rest_exception& rest_exception) {
+      logger_->error("Failed to get streaming messages while clearing streaming status. Exception: '{}'", rest_exception.what());
+      throw;
     }
 
-    streaming_messages = bot_.messages_get_sync(streams_channel_id, {}, earliest_streaming_message_id, {}, 100);
+    for (const auto& streaming_message : streaming_messages) {
+      if (!earliest_streaming_message_id || (streaming_message.first < earliest_streaming_message_id)) {
+        earliest_streaming_message_id = streaming_message.first;
+      }
+
+      bot_.message_delete(streaming_message.first, database_.GetServerChannelId(Database::ServerChannels::kStreams));
+    }
   } while (!streaming_messages.empty());
 }
