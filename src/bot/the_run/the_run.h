bf60a08c61fad58c70953979c872526d5ec321d5
Lucas Folle <4555019+folle@users.noreply.github.com>
2024-05-17 23:03:44 +0200

    Switched websocket from core_client to asio

--- src/bot/the_run/the_run.h
+++ src/bot/the_run/the_run.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <dpp/dpp.h>
-#include <websocketpp/config/core_client.hpp>
+#include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
 
 #include "settings/settings.h"
@@ -16,11 +16,18 @@ public:
   TheRun(std::shared_ptr<Settings> settings, std::shared_ptr<dpp::cluster> bot) noexcept;
 
 private:
+  void OnOpen(websocketpp::client<websocketpp::config::asio_client> *const client, websocketpp::connection_hdl const handler);
+  void OnClose(websocketpp::client<websocketpp::config::asio_client> *const client, websocketpp::connection_hdl const handler);
+  void OnFail(websocketpp::client<websocketpp::config::asio_client> *const client, websocketpp::connection_hdl const handler);
+  void OnMessage(websocketpp::connection_hdl const handler, websocketpp::client<websocketpp::config::asio_client>::message_ptr const message);
+
+private:
   std::shared_ptr<spdlog::async_logger> const logger_ = Logger::Get().Create("The Run");
 
   std::shared_ptr<Settings> const settings_;
   std::shared_ptr<dpp::cluster> const bot_;
 
-  websocketpp::client<websocketpp::config::core_client> client_;
+  websocketpp::client<websocketpp::config::asio_client> client_;
   websocketpp::lib::shared_ptr<websocketpp::lib::thread> thread_;
+  websocketpp::connection_hdl connection_handler_;
 };
